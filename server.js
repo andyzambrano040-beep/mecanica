d16ef356357a40d39323e1d76fe6fa8b4e4e5fbb
andy-zambrano <andyzambrano040@gmail.com>
2026-01-18 17:50:00 -0500

    Refactor server.js to improve routing and port handling

--- server.js
+++ server.js
@@ -1,18 +1,26 @@
 const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
 const app = express();
+const port = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(express.json());
 
-// RUTA BASE
+//Importar rutas
+const authRoutes = require('./routes/auth');
+const vehiculosRoutes = require('./routes/vehiculos');
+
+//usar las rutas
+app.use('/api/auth', authRoutes);
+app.use('/api/vehiculos', vehiculosRoutes);
+
+//Rutas de ejemplos
 app.get('/', (req, res) => {
-  res.send('API funcionando');
+    res.send('Hola desde el servidor Express')
 });
 
-// AUTH (COMÚN)
-const authRoutes = require('./routes/auth');
-app.use('/api/auth', authRoutes);
-
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en puerto ${PORT}`);
+//Iniciar el servidor
+app.listen(port,() => {
+    console.log(`Servidor escuchando en el puerto ${port}`)
 });
